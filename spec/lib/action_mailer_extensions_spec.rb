371f699549b1eaecb2850c1ac15c4c6edd6fe980
Denis Lifanov <inadsence@gmail.com>
2012-04-09 12:53:03 +0400

    Specs for Mailing::ActionMailerExtensions

---
+++ spec/lib/action_mailer_extensions_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe Mailing::ActionMailerExtensions do
+  before(:each) do
+    @template = Factory(:simple_mailing_template)
+  end
+
+  context "when format of the template is HTML" do
+    before { @template.update_attributes(:only_text => false) }
+
+    context "when template disabled" do
+      before(:each) do
+        @template.update_attributes(enabled: false)
+        @message = Mailing::TestMailer.simple('fox', 'dog')
+      end
+
+      specify { @template.should_not be_enabled }
+      it_should_behave_like 'disabled template'
+    end
+
+    context "when template enabled" do
+      before(:each) do
+        @template.update_attributes(enabled: true)
+        @message = Mailing::TestMailer.simple('fox', 'dog')
+      end
+
+      specify { @template.should be_enabled }
+      it_should_behave_like 'multipart template'
+    end
+  end
+
+  context "when format of the template is text" do
+    before { @template.update_attributes(:only_text => true) }
+
+    context "when template disabled" do
+      before(:each) do
+        @template.update_attributes(enabled: false)
+        @message = Mailing::TestMailer.simple('fox', 'dog')
+      end
+
+      specify { @template.should_not be_enabled }
+      it_should_behave_like 'disabled template'
+    end
+
+    context "when template enabled" do
+      before(:each) do
+        @template.update_attributes(enabled: true)
+        @message = Mailing::TestMailer.simple('fox', 'dog')
+      end
+
+      specify { @template.should be_enabled }
+      it_should_behave_like 'text template'
+    end
+  end
+end
